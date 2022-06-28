e49ad50d53a30d7badcec41455427ff94cf33d3d
raza-99 <81908867+raza-99@users.noreply.github.com>
2022-06-28 14:59:53 +0530

    Add files via upload

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { getByText, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-describe('Tests for Counter App',()=>{
-    it('Should have initial value 0', () => {
-      render(<App />);
-      /** 
-       * Todo:
-       * 1. Assert that the initial value is 0. Use getBy/findBy Query to find text '0' from the screen.
-       * 2. use getByRole/findByRole query to find 'increment' button.
-       * 3. Use userEvent to click on increment button to increment the count by 1.
-       * 4. Assert that the count has value incremented by 1. i.e. the current value should become 1.
-       * 5. Similarly perform actions 3 and 4 for decrement.
-       * 
-      */
-    });
-})
+describe("Tests for Counter App", () => {
+  it("Should have initial value 0", () => {
+    render(<App />);
+    /**
+     * Todo:
+     * 1. Assert that the initial value is 0. Use getBy/findBy Query to find text '0' from the screen.
+     * 2. use getByRole/findByRole query to find 'increment' button.
+     * 3. Use userEvent to click on increment button to increment the count by 1.
+     * 4. Assert that the count has value incremented by 1. i.e. the current value should become 1.
+     * 5. Similarly perform actions 3 and 4 for decrement.
+     *
+     */
+
+    const number = screen.getByText("0");
+    const increment = screen.getByRole("button", { name: "increment" });
+    const decrement = screen.getByRole("button", { name: "decrement" });
+    userEvent.click(increment);
+    expect(number).toHaveTextContent("1");
+    userEvent.click(decrement);
+    expect(number).toHaveTextContent("0");
+  });
+});
